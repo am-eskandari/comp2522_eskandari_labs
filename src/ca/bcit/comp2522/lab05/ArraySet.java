dc6cfb72cf7f2f2d6e2c5d0f95f0de7dff05a1fc
Amir Eskandari <113070116+am-eskandari@users.noreply.github.com>
2023-03-05 19:29:27 -0800

    adding javadocs

--- src/ca/bcit/comp2522/lab05/ArraySet.java
+++ src/ca/bcit/comp2522/lab05/ArraySet.java
@@ -2,27 +2,53 @@ package ca.bcit.comp2522.lab05;
 
 import java.util.*;
 
+/**
+ * ArraySet class is an implementation of Set interface that uses an array to store its elements.
+ *
+ * @param <E> the type of elements in the set.
+ * @author Amir Eskandari
+ * @version 1.0
+ */
 public class ArraySet<E> implements Set<E>, Iterable<E> {
 
     private int capacity;
     private int size;
     private E[] elements;
 
+    /**
+     * Constructs an empty ArraySet with an initial capacity of 10.
+     */
     public ArraySet() {
         capacity = 10;
         size = 0;
         elements = (E[]) new Object[capacity];
     }
 
+    /**
+     * Returns the number of elements in this set.
+     *
+     * @return the number of elements in this set.
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * Returns true if this set contains no elements.
+     *
+     * @return true if this set contains no elements.
+     */
     public boolean isEmpty() {
         return size == 0;
     }
 
-    public boolean contains(Object o) {
+    /**
+     * Returns true if this set contains the specified element.
+     *
+     * @param o the element to be checked for containment in this set.
+     * @return true if this set contains the specified element.
+     */
+    public boolean contains(final Object o) {
         for (int i = 0; i < size; i++) {
             if (Objects.equals(o, elements[i])) {
                 return true;
@@ -31,6 +57,11 @@ public class ArraySet<E> implements Set<E>, Iterable<E> {
         return false;
     }
 
+    /**
+     * Returns an iterator over the elements in this set.
+     *
+     * @return an iterator over the elements in this set.
+     */
     public Iterator<E> iterator() {
         return new ArraySetIterator();
     }
@@ -39,10 +70,21 @@ public class ArraySet<E> implements Set<E>, Iterable<E> {
 
         private int currentIndex = 0;
 
+        /**
+         * Returns true if the iteration has more elements.
+         *
+         * @return true if the iteration has more elements.
+         */
         public boolean hasNext() {
             return currentIndex < size;
         }
 
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration.
+         * @throws NoSuchElementException if the iteration has no more elements.
+         */
         public E next() {
             if (!hasNext()) {
                 throw new NoSuchElementException();
@@ -50,16 +92,36 @@ public class ArraySet<E> implements Set<E>, Iterable<E> {
             return elements[currentIndex++];
         }
 
+        /**
+         * Removes from the underlying collection the last element returned by this iterator.
+         *
+         * @throws UnsupportedOperationException always thrown by this implementation of iterator.
+         */
         public void remove() {
             throw new UnsupportedOperationException();
         }
     }
 
+    /**
+     * Returns an array containing all elements in this set.
+     *
+     * @return an array containing all elements in this set.
+     */
     public Object[] toArray() {
         return Arrays.copyOf(elements, size);
     }
 
-    public <T> T[] toArray(T[] a) {
+    /**
+     * Returns an array containing all elements in this set.
+     *
+     * @param <T> the type of the array elements.
+     * @param a   the array into which the elements of this set are to be stored
+     * @return an array containing all elements in this set.
+     * @throws ArrayStoreException  if the runtime type of the specified array is not a supertype of the runtime
+     *                              type of every element in this set.
+     * @throws NullPointerException if the specified array is null.
+     */
+    public <T> T[] toArray(final T[] a) {
         if (a.length < size) {
             return (T[]) Arrays.copyOf(elements, size, a.getClass());
         }
@@ -70,7 +132,13 @@ public class ArraySet<E> implements Set<E>, Iterable<E> {
         return a;
     }
 
-    public boolean add(E e) {
+    /**
+     * Adds the specified element to this set if it is not already present.
+     *
+     * @param e the element to be added to this set.
+     * @return true if this set did not already contain the specified element.
+     */
+    public boolean add(final E e) {
         if (contains(e)) {
             return false;
         }
@@ -82,7 +150,13 @@ public class ArraySet<E> implements Set<E>, Iterable<E> {
         return true;
     }
 
-    public boolean remove(Object o) {
+    /**
+     * Removes the specified element from this set if it is present.
+     *
+     * @param o the element to be removed from this set, if present.
+     * @return true if the set contained the specified element.
+     */
+    public boolean remove(final Object o) {
         for (int i = 0; i < size; i++) {
             if (Objects.equals(o, elements[i])) {
                 elements[i] = elements[--size];
@@ -93,7 +167,13 @@ public class ArraySet<E> implements Set<E>, Iterable<E> {
         return false;
     }
 
-    public boolean containsAll(Collection<?> c) {
+    /**
+     * Returns true if this set contains all elements of the specified collection.
+     *
+     * @param c collection to be checked for containment in this set.
+     * @return true if this set contains all elements of the specified collection.
+     */
+    public boolean containsAll(final Collection<?> c) {
         for (Object o : c) {
             if (!contains(o)) {
                 return false;
@@ -102,7 +182,13 @@ public class ArraySet<E> implements Set<E>, Iterable<E> {
         return true;
     }
 
-    public boolean addAll(Collection<? extends E> c) {
+    /**
+     * Adds all elements in the specified collection to this set if they're not already present.
+     *
+     * @param c collection containing elements to be added to this set.
+     * @return true if this set changed as a result of the call.
+     */
+    public boolean addAll(final Collection<? extends E> c) {
         boolean modified = false;
         for (E e : c) {
             if (add(e)) {
@@ -112,7 +198,14 @@ public class ArraySet<E> implements Set<E>, Iterable<E> {
         return modified;
     }
 
-    public boolean retainAll(Collection<?> c) {
+    /**
+     * Retains only the elements in this set that are contained in the specified collection.
+     * In other words, removes from this set all of its elements that are not contained in the specified collection.
+     *
+     * @param c collection containing elements to be retained in this set.
+     * @return true if this set changed as a result of the call.
+     */
+    public boolean retainAll(final Collection<?> c) {
         boolean modified = false;
         for (int i = 0; i < size; i++) {
             if (!c.contains(elements[i])) {
@@ -124,7 +217,13 @@ public class ArraySet<E> implements Set<E>, Iterable<E> {
         return modified;
     }
 
-    public boolean removeAll(Collection<?> c) {
+    /**
+     * Removes from this set all of its elements that are contained in the specified collection.
+     *
+     * @param c collection containing elements to be removed from this set.
+     * @return true if this set changed as a result of the call.
+     */
+    public boolean removeAll(final Collection<?> c) {
         boolean modified = false;
         for (Object o : c) {
             modified |= remove(o);
@@ -132,6 +231,9 @@ public class ArraySet<E> implements Set<E>, Iterable<E> {
         return modified;
     }
 
+    /**
+     * Removes all elements from this set.
+     */
     public void clear() {
         Arrays.fill(elements, null);
         size = 0;
